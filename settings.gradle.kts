d17b22e3c2c59d00efedc16336850212ed16751e
choidongkuen <danaver12@daum.net>
2024-02-26 15:58:47 +0900

    :wrench: chore: setting.gradle 공통 플러그인 설정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+pluginManagement {
+    plugins {
+        id("io.spring.dependency-management") version "1.1.4"
+        id("org.springframework.boot") version "3.2.2"
+        kotlin("jvm") version "1.9.22"
+        kotlin("plugin.spring") version "1.9.22"
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    }
 }
 rootProject.name = "websocket"
 include("websocket-client")
